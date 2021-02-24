8e6e99eac51d8bb635f345aea19d9a31e3e57e71
ysh8361 <sea5727@naver.com>
2021-02-25 03:05:07 +0900

    commit

--- test/test_odbcconnect.cpp
+++ test/test_odbcconnect.cpp
@@ -16,6 +16,17 @@ public:
     odbctemplate::String SC_TIME;
     odbctemplate::NullString SEND_TIME;
     odbctemplate::NullString IN_SIP_URI;
+    void
+    print(int index){
+        printf("[%d] MSG_SEQ:%d, PROC_RESULT:%d, IN_SECT:%d, SC_TIME:%s, SEND_TIME:%s, IN_SIP_URI:%s\n", 
+            index,
+            MSG_SEQ.Int64, 
+            PROC_RESULT.Valid() == true ? PROC_RESULT.Int64 : 0,
+            IN_SECT.Valid() == true ? IN_SECT.Int64 : 0,
+            SC_TIME.String.c_str(),
+            SEND_TIME.Valid() == true ? SEND_TIME.String.c_str() : "",
+            IN_SIP_URI.Valid() == true ? IN_SIP_URI.String.c_str() : "");
+    }
 };
 
 class TSMS_HISTORY2{
@@ -133,6 +144,104 @@ int main(int argc, char* argv[]) {
         while(fetcher.fetch()){
             // ret.print();
         }
+    }
+
+    {
+        //mutlithread test
+        std::vector<std::thread> workers;
+        workers.emplace_back([]{
+            auto conn = odbctemplate::OdbcConnectBuilder()
+                .setAutocommit(true)
+                .setLoginTimeout(30)
+                .setDsn("DSN=TST_DB;")
+                .build();
+            int thredhold = 10590;
+            int limit = 50;
+            auto stmt = conn.preparedStmt("select MSG_SEQ, PROC_RESULT, IN_SECT, SC_TIME, SEND_TIME, IN_SIP_URI from TSMS_HISTORY where MSG_SEQ > ? limit ?;");
+
+            for(int i = 0 ; i < 100 ; i++){
+                auto ret1 = stmt.bindExecute(thredhold, limit)
+                    .fetch<TSMS_HISTORY>([](odbctemplate::FetchHelper & helper){
+                    TSMS_HISTORY ret;
+                    ret.MSG_SEQ = helper.getInt64();
+                    ret.PROC_RESULT = helper.getNullInt64();
+                    ret.IN_SECT = helper.getNullInt64();
+                    ret.SC_TIME = helper.getString();
+                    ret.SEND_TIME = helper.getNullString();
+                    ret.IN_SIP_URI = helper.getNullString();
+                    return ret;
+                }, limit);
+                
+                for(auto & ret : ret1){
+                    ret.print(1);
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
+        });
+        workers.emplace_back([]{
+            auto conn = odbctemplate::OdbcConnectBuilder()
+                .setAutocommit(true)
+                .setLoginTimeout(30)
+                .setDsn("DSN=TST_DB;")
+                .build();
+            int thredhold = 10590;
+            int limit = 50;
+            auto stmt = conn.preparedStmt("select MSG_SEQ, PROC_RESULT, IN_SECT, SC_TIME, SEND_TIME, IN_SIP_URI from TSMS_HISTORY where MSG_SEQ > ? limit ?;");
+
+            for(int i = 0 ; i < 100 ; i++){
+                auto ret1 = stmt.bindExecute(thredhold, limit)
+                    .fetch<TSMS_HISTORY>([](odbctemplate::FetchHelper & helper){
+                    TSMS_HISTORY ret;
+                    ret.MSG_SEQ = helper.getInt64();
+                    ret.PROC_RESULT = helper.getNullInt64();
+                    ret.IN_SECT = helper.getNullInt64();
+                    ret.SC_TIME = helper.getString();
+                    ret.SEND_TIME = helper.getNullString();
+                    ret.IN_SIP_URI = helper.getNullString();
+                    return ret;
+                }, limit);
+                
+                for(auto & ret : ret1){
+                    ret.print(2);
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
+        });
+        workers.emplace_back([]{
+            auto conn = odbctemplate::OdbcConnectBuilder()
+                .setAutocommit(true)
+                .setLoginTimeout(30)
+                .setDsn("DSN=TST_DB;")
+                .build();
+            int thredhold = 10590;
+            int limit = 50;
+            auto stmt = conn.preparedStmt("select MSG_SEQ, PROC_RESULT, IN_SECT, SC_TIME, SEND_TIME, IN_SIP_URI from TSMS_HISTORY where MSG_SEQ > ? limit ?;");
+
+            for(int i = 0 ; i < 100 ; i++){
+                auto ret1 = stmt.bindExecute(thredhold, limit)
+                    .fetch<TSMS_HISTORY>([](odbctemplate::FetchHelper & helper){
+                    TSMS_HISTORY ret;
+                    ret.MSG_SEQ = helper.getInt64();
+                    ret.PROC_RESULT = helper.getNullInt64();
+                    ret.IN_SECT = helper.getNullInt64();
+                    ret.SC_TIME = helper.getString();
+                    ret.SEND_TIME = helper.getNullString();
+                    ret.IN_SIP_URI = helper.getNullString();
+                    return ret;
+                }, limit);
+                
+                for(auto & ret : ret1){
+                    ret.print(3);
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
+        });
+
+        for(auto & worker : workers){
+            printf("join ? \n");
+            worker.join();
+        }
+
     }
     return 0;
 
