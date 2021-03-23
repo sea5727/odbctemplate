91e41ea6f66489e55f7c2fe2b0e6af85fd2c848d
ysh8361 <sea5727@naver.com>
2021-03-23 14:06:24 +0900

    commit

--- odbc_error.hpp
+++ odbc_error.hpp
@@ -106,7 +106,7 @@ namespace odbctemplate
                                     (SQLCHAR *)text,
                                     sizeof(text), 
                                     &len );
-                std::cout << "state:" << state << "text:" << text << std::endl;
+                // std::cout << "state:" << state << "text:" << text << std::endl;
                 if(ret != SQL_SUCCESS)
                     break;
                 
